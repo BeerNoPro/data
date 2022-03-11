8f17f68da6560304b8d737e488d751ed7436e320
Pham Viet Hung <hungpv@unitech.net>
2022-03-11 15:59:33 +0700

    add-data-sql

--- sercurity_data_ok.sql
+++ sercurity_data_ok.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 01, 2022 at 05:20 AM
+-- Generation Time: Mar 11, 2022 at 09:58 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -43,8 +43,9 @@ CREATE TABLE `carbinet` (
 
 INSERT INTO `carbinet` (`id`, `name`, `work_room_id`, `member_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
 (1, 'shoe cabinet', 1, 2, '2022-02-18 01:12:00', '2022-02-28 20:43:30', NULL),
-(2, 'shoes cabinet', 5, 3, '2022-02-18 01:12:10', '2022-02-28 20:44:01', '1'),
-(3, 'Table', 1, 1, '2022-02-28 20:43:10', '2022-02-28 20:43:19', NULL);
+(2, 'name', 6, 4, '2022-02-18 01:12:10', '2022-03-10 21:25:50', '1'),
+(3, 'Table', 1, 1, '2022-02-28 20:43:10', '2022-02-28 20:43:19', NULL),
+(4, 'clothes', 4, 5, '2022-03-10 21:24:57', '2022-03-10 21:24:57', NULL);
 
 -- --------------------------------------------------------
 
@@ -75,7 +76,8 @@ INSERT INTO `company` (`id`, `name`, `address`, `email`, `phone`, `date_incorpor
 (7, 'FPT company', '02 Quang Trung', 'fpt@gmail.com', 936567666, '2000-07-02', '2022-02-20 20:20:02', '2022-02-23 23:55:52', NULL),
 (8, 'Software company', '02 Quang Trung', 'software@gmail.com', 988234567, '2022-02-25', '2022-02-23 18:59:08', '2022-02-24 01:25:27', NULL),
 (9, 'Fsoft company', '05 Nguyen Tat Thanh', 'fsoft@gmail.com', 977888999, '2022-02-08', '2022-02-26 23:20:22', '2022-02-26 23:21:19', NULL),
-(10, 'Tech company', '02 Quang Khai', 'tech@gmail.com', 936567669, '2022-02-11', '2022-02-26 23:21:04', '2022-02-26 23:21:04', NULL);
+(10, 'Tech company', '02 Quang Khai', 'tech@gmail.com', 936567669, '2022-02-11', '2022-02-26 23:21:04', '2022-02-26 23:21:04', NULL),
+(11, 'Da Nang software', '02 Quang Trung', 'danang@gmail.com', 936567669, '2022-03-10', '2022-03-09 20:32:06', '2022-03-09 21:42:45', NULL);
 
 -- --------------------------------------------------------
 
@@ -102,10 +104,11 @@ CREATE TABLE `device` (
 --
 
 INSERT INTO `device` (`id`, `ip_address`, `ip_mac`, `user_login`, `version_win`, `version_virus`, `update_win`, `member_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, '192.168.5.128', '192...', 'unitek\\hungpv', 'win 10', '8.0', '2022-03-05', 1, '2022-02-18 00:47:40', '2022-02-18 00:47:40', NULL),
-(2, '192.168.5...', '199...', 'unitek\\minhtn', 'win 11', '8.0', '2022-03-05', 2, '2022-02-18 00:49:00', '2022-02-18 01:04:01', NULL),
+(1, '192.168.5.128', '192...', 'unitek.hungpv', 'win 10', '8.0', '2022-03-05', 1, '2022-02-18 00:47:40', '2022-03-05 19:19:17', NULL),
+(2, '192.168.5...', '199...', 'unitek.minhtn', 'win 11', '8.0', '2022-03-05', 2, '2022-02-18 00:49:00', '2022-03-05 19:19:33', NULL),
 (3, '199,645,756', '56745', 'unitek\\cuong', '10', '8', '2022-03-23', 3, '2022-02-28 19:34:01', '2022-02-28 19:36:03', NULL),
-(4, '12314', '12445', 'manh', '11', '10', '2022-03-18', 4, '2022-02-28 20:11:55', '2022-02-28 20:11:55', NULL);
+(4, '12314', '12445', 'manh', '11', '10', '2022-03-18', 4, '2022-02-28 20:11:55', '2022-02-28 20:11:55', NULL),
+(5, '123', '123123', 'tai 2', '11', '11', '2022-03-10', 8, '2022-03-10 20:59:51', '2022-03-10 21:01:28', NULL);
 
 -- --------------------------------------------------------
 
@@ -154,9 +157,11 @@ INSERT INTO `member` (`id`, `name`, `email`, `passwork`, `address`, `phone_numbe
 (2, 'Tran Anh Minh', 'minh@unitech.vn', NULL, '02 Quang Trung', 988778899, 'Dev', '2022-01-14', NULL, '2022-02-16 20:00:26', '2022-02-16 20:00:26', 1, NULL),
 (3, 'Cao Chi Cuong', 'cuong@gmail.com', NULL, '77 Quang Trung', 936567666, 'Dev', '2022-03-05', NULL, '2022-02-20 20:47:48', '2022-02-20 20:47:48', 4, NULL),
 (4, 'Tran Nguyen Manh', 'manh@gmail.com', NULL, '07 Nguyen Luong Bang', 988777444, 'Dev', '2022-02-02', NULL, NULL, NULL, 8, NULL),
-(5, 'Doan Nguyen Duc', 'doannguyenduc@gmail.com', NULL, '02 Quang Trung', 936567669, 'PM', '2022-02-08', NULL, '2022-02-26 21:00:35', '2022-02-26 23:13:12', 7, NULL),
+(5, 'Nguyen Duc', 'duc@gmail.com', NULL, '02 Quang Trung', 936567669, 'PM', '2022-02-08', NULL, '2022-02-26 21:00:35', '2022-03-10 01:46:51', 9, NULL),
 (6, 'Cao Tuan Anh', 'caotuananh@gmail.com', NULL, '02 Quang Trung', 936567669, 'Dev', '2022-02-09', NULL, '2022-02-26 21:02:22', '2022-02-26 21:02:22', 8, NULL),
-(7, 'Tran Ngoc Hai', 'tranngochai@gmail.com', NULL, '02 Dinh Tien Hoang', 936567669, 'Dev', '2022-02-15', NULL, '2022-02-26 21:03:25', '2022-02-26 21:03:25', 3, NULL);
+(7, 'Tran Ngoc Hai', 'tranngochai@gmail.com', NULL, '02 Dinh Tien Hoang', 936567669, 'Dev', '2022-02-15', NULL, '2022-02-26 21:03:25', '2022-02-26 21:03:25', 3, NULL),
+(8, 'Cao  Anh Tai', 'tai@gmail.com', NULL, '05 Nguyen Huu Tho', 988666888, 'Dev', '2022-02-02', NULL, '2022-03-01 01:16:17', '2022-03-01 01:16:17', 1, NULL),
+(9, 'Cao Hai Anh', 'anh@gmail.com', NULL, '02 Quang Trung', 936567669, 'Dev', '2022-03-10', NULL, '2022-03-10 01:33:35', '2022-03-10 01:45:54', 7, NULL);
 
 -- --------------------------------------------------------
 
@@ -182,11 +187,13 @@ CREATE TABLE `member_project` (
 INSERT INTO `member_project` (`member_id`, `project_id`, `role`, `time_member_join`, `time_member_completed`, `created_at`, `updated_at`, `deleted_at`) VALUES
 (1, 1, 'Dev', '2022-03-10', '2022-06-10', '2022-02-16 23:47:48', '2022-02-27 21:02:03', NULL),
 (2, 2, 'BA', '2022-03-10', '2022-06-10', '2022-02-17 00:08:42', '2022-02-18 18:47:21', NULL),
+(2, 5, 'PM', '2022-02-04', '2022-06-04', '2022-03-01 01:39:57', '2022-03-10 19:51:51', NULL),
 (3, 3, 'Dev', '2022-03-10', '2022-06-10', '2022-02-16 23:49:07', '2022-02-27 21:10:15', NULL),
-(3, 4, 'BA', '2022-02-02', '2022-02-26', '2022-02-27 21:13:57', '2022-02-27 21:13:57', NULL),
+(3, 6, 'Dev', '2022-02-10', '2022-02-26', '2022-02-27 21:13:17', '2022-03-10 19:52:50', NULL),
 (4, 1, 'PM', '2022-02-01', '2022-02-28', '2022-02-27 19:27:14', '2022-02-27 21:11:53', NULL),
-(4, 2, 'Dev', '2022-02-10', '2022-02-26', '2022-02-27 21:13:17', '2022-02-27 21:13:17', NULL),
-(6, 1, 'Dev', '2022-02-02', '2022-02-26', '2022-02-27 21:14:21', '2022-02-27 21:14:21', NULL);
+(5, 6, 'PM', '2022-03-02', '2022-03-30', '2022-03-10 19:31:13', '2022-03-10 19:31:13', NULL),
+(6, 1, 'Dev', '2022-02-02', '2022-02-26', '2022-02-27 21:14:21', '2022-02-27 21:14:21', NULL),
+(6, 7, 'Dev', '2022-02-02', '2022-02-26', '2022-02-27 21:13:57', '2022-03-10 19:52:10', NULL);
 
 -- --------------------------------------------------------
 
@@ -273,9 +280,12 @@ CREATE TABLE `project` (
 
 INSERT INTO `project` (`id`, `name`, `time_start`, `time_completed`, `company_id`, `work_room_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
 (1, 'Sercurity project', '2022-02-10', '2022-06-10', 1, 1, '2022-02-16 21:55:24', '2022-02-27 18:12:34', NULL),
-(2, 'Sale project', '2022-02-10', '2020-05-10', 1, 4, '2022-02-16 22:43:37', '2022-02-27 08:53:42', NULL),
-(3, 'Github project', '2022-01-10', '2022-02-10', 10, 1, '2022-02-27 08:46:55', '2022-02-27 21:07:00', NULL),
-(4, 'Gitlab project', '2022-02-01', '2022-02-28', 1, 3, '2022-02-27 18:12:11', '2022-02-27 20:36:05', NULL);
+(2, 'Sale project', '2022-02-10', '2020-05-10', 7, 4, '2022-02-16 22:43:37', '2022-02-27 08:53:42', NULL),
+(3, 'Github project', '2022-01-10', '2022-02-10', 10, 5, '2022-02-27 08:46:55', '2022-02-27 21:07:00', NULL),
+(4, 'Gitlab project 2', '2022-02-01', '2022-02-28', 1, 1, '2022-02-27 18:12:11', '2022-03-10 19:36:59', NULL),
+(5, 'App weather project', '2022-03-01', '2022-03-31', 1, 1, '2022-03-10 07:59:27', '2022-03-10 07:59:27', NULL),
+(6, 'Google translate', '2022-03-02', '2022-03-30', 3, 4, '2022-03-10 08:01:20', '2022-03-10 08:01:20', NULL),
+(7, 'Google', '2022-03-05', '2022-03-28', 7, 7, '2022-03-10 08:02:39', '2022-03-10 08:02:39', NULL);
 
 -- --------------------------------------------------------
 
@@ -298,11 +308,12 @@ CREATE TABLE `training` (
 --
 
 INSERT INTO `training` (`id`, `trainer`, `project_id`, `content`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 1, 1, 'Learn laravel', '2022-02-17 02:34:29', '2022-02-17 02:34:29', NULL),
+(1, 1, 1, 'Learn laravel and git', '2022-02-17 02:34:29', '2022-03-10 20:21:30', NULL),
 (2, 2, 2, 'Learn javascript', '2022-02-17 02:41:14', '2022-02-17 21:30:19', NULL),
 (3, 6, 1, 'Learn node-js', '2022-02-17 21:14:57', '2022-02-28 00:29:03', '1'),
 (4, 3, 3, 'Learn html', '2022-02-28 00:03:41', '2022-02-28 00:03:41', NULL),
-(5, 4, 3, 'Learn git', '2022-02-28 00:10:48', '2022-02-28 00:10:48', NULL);
+(5, 4, 3, 'Learn git', '2022-02-28 00:10:48', '2022-02-28 00:10:48', NULL),
+(6, 5, 7, 'Learn javascript', '2022-03-10 20:17:25', '2022-03-10 20:21:11', NULL);
 
 -- --------------------------------------------------------
 
@@ -327,11 +338,12 @@ CREATE TABLE `training_room` (
 --
 
 INSERT INTO `training_room` (`training_id`, `member_id`, `date_start`, `date_completed`, `result`, `note`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 5, '2022-02-01', '2022-02-15', 'hi', 'hi', '2022-02-28 07:23:07', '2022-02-28 07:23:07', NULL),
-(2, 1, '2022-02-01', '2022-02-10', 'ok ok', 'ok ok', '2022-02-17 23:34:20', '2022-02-28 08:45:47', NULL),
+(1, 5, '2022-02-01', '2022-02-15', 'ok', 'hi', '2022-02-28 07:23:07', '2022-03-07 00:06:42', NULL),
+(2, 1, '2021-02-04', '2022-04-14', 'fine', 'ok', '2022-02-17 23:33:02', '2022-03-11 01:35:12', NULL),
 (2, 4, '2022-02-01', '2022-02-15', 'ha', 'ha', '2022-02-28 07:13:39', '2022-02-28 08:56:42', NULL),
-(3, 2, '2021-02-04', '2022-04-14', 'fine', 'You need try again', '2022-02-17 23:33:02', '2022-02-20 23:34:53', NULL),
 (3, 3, '2022-02-01', '2022-02-10', 'ok', 'ok', '2022-02-28 07:34:59', '2022-02-28 07:34:59', NULL),
+(3, 4, '2022-03-01', '2022-03-10', 'ok', 'ok', '2022-03-11 01:30:17', '2022-03-11 01:30:17', NULL),
+(3, 6, '2022-02-01', '2022-02-10', 'ok', 'ok', '2022-02-17 23:34:20', '2022-03-11 01:35:23', NULL),
 (4, 7, '2022-02-02', '2022-02-16', 'Training ok', 'ok ok!', '2022-02-28 07:16:28', '2022-02-28 07:16:28', NULL);
 
 -- --------------------------------------------------------
@@ -371,10 +383,13 @@ CREATE TABLE `work_room` (
 --
 
 INSERT INTO `work_room` (`id`, `name`, `location`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'Floor 7', '02 Quang Trung', '2022-02-15 23:51:32', '2022-02-25 02:16:42', NULL),
-(3, 'Tâng 8 khu công viên phần mềm', '02 Quang Trung', '2022-02-16 02:10:52', '2022-02-17 20:00:25', '1'),
-(4, 'Tâng 7 khu công viên phần mềm', '02 Quang Trung', '2022-02-16 19:02:03', '2022-02-16 19:06:28', NULL),
-(5, 'Floor 5', '07 Nguyen Huu Tho', '2022-02-25 02:14:30', '2022-02-25 02:14:30', NULL);
+(1, '07th Floor - Dang Nang Software Park', '02 Quang Trung St', '2022-02-15 23:51:32', '2022-03-02 23:59:31', NULL),
+(3, '10th Floor', '02 Quang Trung St', '2022-02-16 02:10:52', '2022-03-03 00:01:18', NULL),
+(4, '02th Floor', '02 Nguyen Van Linh St', '2022-02-16 19:02:03', '2022-03-03 00:01:25', NULL),
+(5, '05th Floor', '07 Nguyen Huu Tho St', '2022-02-25 02:14:30', '2022-03-03 00:01:31', NULL),
+(6, '08th Floor', '09 Phan Thanh St', '2022-03-10 00:03:09', '2022-03-10 00:10:54', NULL),
+(7, '03th Floor', '09 Tran Phu St...', '2022-03-10 00:06:32', '2022-03-10 00:12:31', NULL),
+(8, '15th Floor', '77 Vo Nguyen Giap', '2022-03-10 00:13:20', '2022-03-10 00:13:20', NULL);
 
 --
 -- Indexes for dumped tables
@@ -486,19 +501,19 @@ ALTER TABLE `work_room`
 -- AUTO_INCREMENT for table `carbinet`
 --
 ALTER TABLE `carbinet`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `device`
 --
 ALTER TABLE `device`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -510,7 +525,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -528,13 +543,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `training`
 --
 ALTER TABLE `training`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -546,7 +561,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `work_room`
 --
 ALTER TABLE `work_room`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
